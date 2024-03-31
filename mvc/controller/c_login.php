5c54b0295df69d74c939fa565a63966d9c717aa4
unknown <nhtuanhao@gmail.com>
2024-03-31 20:15:41 +0700

    fix cau truc

--- mvc/controller/c_login.php
+++ mvc/controller/c_login.php
@@ -1,3 +1,13 @@
 <?php
-    require_once "mvc/view/v_login.php";
+    require "mvc/view/v_login.php";
+    require "mvc/model/m_login.php";
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Lấy dữ liệu từ form
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        // Gọi hàm login
+        login($username, $password);
+    }
 ?>
--- mvc/view/v_login.php
+++ mvc/view/v_login.php
@@ -1,17 +1,3 @@
-<?php
-    include 'mvc/model/m_login.php';
-    
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Lấy dữ liệu từ form
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        
-        
-        // Gọi hàm login
-        login($username, $password);
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
