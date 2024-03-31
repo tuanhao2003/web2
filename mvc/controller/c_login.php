0bbbead632eea107b3d56e3771b3050531e4d751
unknown <nhtuanhao@gmail.com>
2024-03-31 20:36:42 +0700

    update nut bam dang nhap

--- mvc/controller/c_login.php
+++ mvc/controller/c_login.php
@@ -1,7 +1,7 @@
 <?php
-    require "mvc/view/v_login.php";
-    require "mvc/model/m_login.php";
-    
+    require_once "mvc/view/v_login.php";
+    require_once "mvc/model/m_login.php";
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Lấy dữ liệu từ form
         $username = $_POST['username'];
